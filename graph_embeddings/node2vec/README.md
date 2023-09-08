dcc6c7a2286e5b48fc57dbe40459267e3dc7317a
paulosantosneto <PauloSantosNeto18@gmail.com>
2023-09-08 17:20:23 -0300

    implementation of node2vec

---
+++ graph_embeddings/node2vec/README.md
@@ -0,0 +1,12 @@
+`Warning` This is my implementation of the article ['node2vec: Scalable Feature Learning for Networks'](https://arxiv.org/pdf/1607.00653.pdf). Therefore, it is not an official repository of it. Any code improvement recommendations are welcome.
+
+To train the model:
+```
+python3 node2vec.py --mode train --number_nodes 50 --percent_edges 0.1 --p 0.2 --q 0.8
+```
+
+To infer:
+```
+python3 node2vec.py --mode inference --model_load_path weights_50.pth --model_config_path parameters.json --node 0
+```
+
---
+++ graph_embeddings/node2vec/utils.py
@@ -0,0 +1,128 @@
+import os
+import torch
+import json
+import networkx as nx
+import igraph as ig
+import matplotlib.pyplot as plt
+import argparse
+from sklearn.decomposition import PCA
+import numpy as np
+
+def get_args():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', default=50, type=int, help='number of epochs.')
+    parser.add_argument('--embedding_size', default=100, type=int, help='the latent dimension or latent space to which the nodes will be reduced.')
+    parser.add_argument('--mode', default='train', type=str, help='the execution mode: training or inference.')
+    parser.add_argument('--l', default=4, type=int, help='walk legth.')
+    parser.add_argument('--r', default=3, type=int, help='walks per node.')
+    parser.add_argument('--k', default=2, type=int, help='context size.')
+    parser.add_argument('--number_nodes', default=50, type=int, help='number of nodes in graph.')
+    parser.add_argument('--percent_edges', default=0.5, type=float, help='percentage of connections between nodes.')
+    parser.add_argument('--model_load_path', type=str)
+    parser.add_argument('--model_config_path', type=str)
+    parser.add_argument('--p', default=0.5, type=float, help='out parameter.')
+    parser.add_argument('--q', default=0.5, type=float, help='In-out parameter.')
+     
+    args = parser.parse_args()
+
+    return args
+
+def save_parameters(vocab_size: int, embedding_size: int, context_size: int):
+    
+    params = {
+            'vocab_size': vocab_size,
+            'embedding_size': embedding_size,
+            'context_size': context_size
+    }
+
+    with open('weights/parameters.json', 'w') as json_config:
+        json.dump(params, json_config)
+
+def load_parameters(config_path):
+    
+    params = {}
+
+    with open(f'weights/{config_path}', 'r') as json_config:
+        params = json.load(json_config)
+
+    return params
+
+def load_graph(path: str):
+    ''' Load the json graph file '''
+    
+    graph = {}
+
+    with open(f'{path}') as f:
+        graph = json.load(f)
+
+    return graph
+
+def save_model(model: any, epochs: str):
+
+    try:
+        if not os.path.exists('weights'):
+            os.makedirs('weights')
+
+        torch.save(model.state_dict(), f'weights/weights_{epochs}.pth')
+
+        return 'Model was successfully saved!'
+    except:
+        raise 'There was a problem saving the model!'
+
+def plot_pca(vocab: list, data: list):
+
+    pca = PCA(n_components=2)
+    data_pca = pca.fit_transform(data)
+
+    plt.scatter(data_pca[:, 0], data_pca[:, 1])
+
+    for i, word in enumerate(vocab):
+        plt.annotate(word, (data_pca[i, 0], data_pca[i, 1]), fontsize=8)
+
+    plt.savefig('PCA.png')
+
+def plot_loss(epochs: int, loss_history: list):
+
+    plt.clf()
+    plt.plot(np.arange(epochs), loss_history)
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.savefig('loss.png')
+    plt.clf()
+
+def edges_nodes(graph: list):
+
+    nodes = []
+
+    for nds in graph:
+        nodes.extend(nds)
+
+    nodes = set(nodes)
+
+    return nodes, graph
+
+def list2dict(nodes: list, edges: list):
+
+    graph = {i: [] for i in nodes}
+
+    for node in nodes:
+        for edge in edges:
+            if edge[1] not in graph[edge[0]]:
+                graph[edge[0]].append(edge[1])
+            if edge[0] not in graph[edge[1]]:
+                graph[edge[1]].append(edge[0])
+
+    return graph
+
+def plot_graph(nodes: list, edges: list):
+
+    g = nx.Graph()
+    g.add_edges_from(edges)
+    g.add_nodes_from(nodes)
+    layout = nx.circular_layout(g)
+    nx.draw(g, alpha=0.9, node_size=1000, node_color='black', pos=layout, with_labels=True, font_color='whitesmoke')
+    plt.savefig(os.getcwd() + '/graph.png')
+
+
+
